34cd46b6c51dd2035c3cc9aa9671da4d35e6446b
ahiru60 <adhirunath@gmail.com>
2024-07-10 12:23:14 +0530

    changes

---
+++ functions/api.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const bodyParser = require('body-parser');
+const userController = require('..src/userController');
+const authController = require('..src/authController');
+const furnitureController = require('..src/furnitureController');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+
+router.get('/users', userController);
+router.get('/auth', authController);
+router.get('/furniture', furnitureController);
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
